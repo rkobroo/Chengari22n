8cfe7de326632442bdf114fe29aeec4be9898d8e
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-20 15:13:56 +0545

    Update 000unsendno.js

--- modules/commands/000unsendno.js
+++ modules/commands/000unsendno.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const fs = require('fs-extra');
 
 module.exports.config = {
-	name: "😑",
+	name: "uns",
 	version: "1.0.0", 
 	hasPermssion: 2,
 	credits: "hulaanmo",//Riyad
