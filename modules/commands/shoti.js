74996f2cbb05ac0020c8617eed8284f6fdf3c12e
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-04-22 00:18:19 +0545

    Update shoti.js

--- modules/commands/shoti.js
+++ modules/commands/shoti.js
@@ -1,50 +1,31 @@
-module.exports.run = {
-name: "shoti",
-version: "1.0.0",
-hasPermission: 0,
-Credit: "libyzxy0",//convert Kaizenji
-Description: { en: "Generate a random tiktok video."},
-commandCategory: "fun",
-coolDown: 0,
-},
+module.exports.config = {
+  name: "shoti",
+  version: "1.0.0",
+  credits: "libyzxy0",
+  description: "Generate random tiktok girl videos",
+  hasPermssion: 0,
+  commandCategory: "other",
+  usage: "[shoti]",
+  cooldowns: 5,
+  dependencies: [],
+  usePrefix: true
+};
 
-Run: async ({ api, event, args }) => {
-
-api.setMessageReaction("⏳", event.messageID, (err) => {
-}, true);
-api.sendTypingIndicator(event.threadID, true);
-
-const { messageID, threadID } = event;
-const fs = require("fs");
-const axios = require("axios");
-const request = require("request");
-const prompt = args.join(" ");
-
-if (!prompt[0]) { api.sendMessage("Sending mga shoti ni GabYu...", threadID, messageID);
-}
-
-try {
-const response = await axios.post(`https://shoti-server-v2.vercel.app/api/v1/get`, { apikey: `$shoti-1hjvb0q3sokk2bvme` });
-
-const path = __dirname + `/cache/shoti.mp4`;
-const file = fs.createWriteStream(path);
-const rqs = request(encodeURI(response.data.data.url));
-rqs.pipe(file);
-file.on(`finish`, () => {
-setTimeout(function() {
-api.setMessageReaction("✅", event.messageID, (err) => {
-}, true);
-return api.sendMessage({
-body: `SHOTI DOWNLOADED! \n\n🍑 •| userName: @${response.data.data.user.username}\n🍑 •| userNickname: ${response.data.data.user.nickname}\n👾 •| userID: ${response.data.data.user.userID}\n👾 •| Duration: ${response.data.data.duration}`, 
-attachment: fs.createReadStream(path)
-}, threadID);
-}, 5000);
-});
-file.on(`error`, (err) => {
-api.sendMessage(`Error: ${err}`, threadID, messageID);
-});
-} catch (err) {
-api.sendMessage(`Error: ${err}`, threadID, messageID);
-}
-}
+module.exports.run = async function({ api, event }) {
+  const axios = require("axios");
+  const request = require('request');
+  const fs = require("fs")
+  let response = await axios.post('https://shoti-api.libyzxy0.repl.co/api/get-shoti', { apikey: "YOUR-API-KEY" });
+  var file = fs.createWriteStream(__dirname + "/cache/shoti.mp4");
+  var rqs = request(encodeURI(response.data.data.url));
+  rqs.pipe(file);
+  file.on('finish', () => {
+    return api.sendMessage({
+      body: `@${response.data.user.username}`, 
+      attachment: fs.createReadStream(__dirname + '/cache/shoti.mp4')
+    }, event.threadID, event.messageID)
+  })
+  file.on('error', (err) => {
+      api.sendMessage(`Shoti Error: ${err}`, event.threadID, event.messageID);
+  })
 };
