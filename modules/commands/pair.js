2bd142586b63e1fbed7bf2e5391f2b564ad2ffbb
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-05-24 13:10:48 +0545

    Update pair.js

--- modules/commands/pair.js
+++ modules/commands/pair.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "pair",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+  credits: "RKO BRO",
   description: "It's a compound :>",
   commandCategory: "Giải trí",
   usages: "",
