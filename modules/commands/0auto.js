c8bffd6e70e3d4d01d04c8d2d2a6ea59c092c67e
rkobroo <156598034+rkobroo@users.noreply.github.com>
2024-06-05 14:08:07 +0545

    Update 0auto.js

--- modules/commands/0auto.js
+++ modules/commands/0auto.js
@@ -1,88 +1,48 @@
-const axios = require('axios');
-const fs = require('fs-extra');
-const d1pt0 = require('tinyurl');
-const path = require('path');
-
 module.exports.config = {
-name: "alldl",
-version: "1.0.",
-role: 0,
-author: "Dipto",
-description: "All Video Downloader",
-category: "other",
-guide: {en:"video link"},
-coolDowns: 0
+  name: "autodl",
+  version: "1.0.",
+  hasPermission: 0,
+  credits: "RKO BRO",
+  description: "All Video Downloader",
+  commandCategory: "Downloader",
+  usages: "[video link]",
+  cooldowns: 0
 };
 
-module.exports.onStart = async function ({ api, event, args }) {
-let dipto = args.join(" ") || (event.messageReply && event.messageReply.attachments.length > 0 ? event.messageReply.attachments[0].url : "");
-if (!dipto) {
-api.sendMessage("No URL provided or found in message reply.", event.threadID, event.messageID);
-return;
-}
+module.exports.handleEvent = async function ({ api, event, client, __GLOBAL }) {
+  const axios = require('axios');
+  const fs = require('fs-extra');
+  let dipto = event.body ? event.body : '';
+  try {
+    if (dipto.startsWith('https://vt.tiktok.com') || dipto.startsWith('https://www.facebook.com') || dipto.startsWith('https://www.instagram.com/') || dipto.startsWith('https://youtu.be/') || dipto.startsWith('https://youtube.com/') || dipto.startsWith('https://x.com/') || dipto.startsWith('https://twitter.com/') || dipto.startsWith('https://vm.tiktok.com') || dipto.startsWith('https://www.capcut.com') || dipto.startsWith('https://fb.watch')) {
+      api.sendMessage("", event.threadID, event.messageID);
+      if (!dipto) {
+        api.sendMessage("please put a valid video link", event.threadID, event.messageID);
+        return;
+      }
 
-const w = await api.sendMessage("Your video is downloading...", event.threadID);
-api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
-
-try {
-if (dipto.startsWith('https://www.facebook.com/reel/')) {
-const response = await axios.get(`${global.GoatBot.config.api}/alldl?url=${encodeURIComponent(dipto)}`);
-const result = response.data;
-const shortUrl = await d1pt0.shorten(result.result);
-api.sendMessage({
-body: `${result.cp}\nVideo URL = ${shortUrl}`,
-attachment: await global.utils.getStreamFromURL(result.result)
-}, event.threadID, event.messageID)
-//async () => {
-api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-await api.unsendMessage(w.messageID);
-
-}
-else if (dipto.startsWith('https://www.facebook.com/') || dipto.startsWith('https://fb.watch')) {
-let vidInfo = event.attachments[0] || event.messageReply.attachments[0];
-const shortUrl = await d1pt0.shorten(vidInfo.playableUrl);
-api.sendMessage({
-body: `description: ${vidInfo.description || 'N/A'}\nsource: ${vidInfo.source || 'N/A'}\nurl: ${shortUrl}`,
-attachment: await global.utils.getStreamFromURL(vidInfo.playableUrl)
-}, event.threadID, event.messageID)
-//, async () => {
-api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-await api.unsendMessage(w.messageID);
-// });
-}
-else if (dipto.match(/https:\/\/(vt|vm)\.tiktok\.com|https:\/\/www\.instagram\.com\/|https:\/\/youtu\.be\/|https:\/\/www\.instagram\.com\/p\/|https:\/\/pin\.it\/|https:\/\/youtube\.com\/|https:\/\/www\.capcut\.com\/|https:\/\/www\.threads\.net\/|https:\/\/twitter\.com\/|https:\/\/x\.com\/|https:\/\/l\.likee\.video\//)) {
-const response = await axios.get(`${global.GoatBot.config.api}/alldl?url=${encodeURIComponent(dipto)}`);
-const result = response.data;
-const shortUrl = await d1pt0.shorten(result.result);
-api.sendMessage({
-body: `${result.cp}\nVideo URL = ${shortUrl}`,
-attachment: await global.utils.getStreamFromURL(result.result)
-}, event.threadID, event.messageID)
-//, async () => {
-api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-await api.unsendMessage(w.messageID);
-// });
-}
-else if (dipto.startsWith('https://i.imgur.com')) {
-const response = await axios.get(dipto, { responseType: 'arraybuffer' });
-const fileExtension = path.extname(dipto);
-const filePath = path.join(__dirname, `/cache/dipto${fileExtension}`);
-fs.writeFileSync(filePath, Buffer.from(response.data, 'binary'));
-api.sendMessage({
-body: "Downloaded from link",
-attachment: fs.createReadStream(filePath)
-}, event.threadID, () => fs.unlinkSync(filePath), event.messageID)
-//, async () => {
-api.setMessageReaction("✅", event.messageID, (err) => {}, true);
-await api.unsendMessage(w.messageID);
-// });
-}
-else {
-api.sendMessage("Unsupported URL or command usage.", event.threadID, event.messageID);
-await api.unsendMessage(w.messageID);
-}
-} catch (e) {
-api.sendMessage(`Error: photo hudaina download`, event.threadID, event.messageID);
-await api.unsendMessage(w.messageID);
-}
+      const aa = await axios.get(`https://noobs-api2.onrender.com/dipto/alldl?url=${encodeURIComponent(dipto)}`);
+      const bb = aa.data;
+      const filePath = __dirname + `/cache/diptoo.mp4`;
+      const vid = (await axios.get(bb.result, { responseType: "arraybuffer" })).data;
+      fs.writeFileSync(filePath, Buffer.from(vid, 'utf-8'));
+      api.sendMessage({
+        body: `${bb.cp} ♻️𝗖𝗥𝗘𝗗𝗜𝗧:𝗥𝗞𝗢 𝗕𝗥𝗢💝💞 `,
+        attachment: fs.createReadStream(filePath)
+      }, event.threadID, () => fs.unlinkSync(filePath), event.messageID)
+    }
+    if (dipto.startsWith('https://i.imgur.com')) {
+      const dipto3 = dipto.substring(dipto.lastIndexOf('.'));
+      const response = await axios.get(dipto, { responseType: 'arraybuffer' });
+      const filename = __dirname + `/cache/dipto${dipto3}`;
+      fs.writeFileSync(filename, Buffer.from(response.data, 'binary'));
+      api.sendMessage({
+        body: `Downloaded from link`,
+        attachment: fs.createReadStream(filename)
+      }, event.threadID, () => fs.unlinkSync(filename), event.messageID)
+    }
+  } catch (e) {
+    api.sendMessage(`Photo download hudaina💝`, event.threadID, event.messageID);
+  }
 };
+module.exports.run = function ({ api, event, client, __GLOBAL }) {}
